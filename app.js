2e053e29f68f8e23d867fbb0a1d6486f67f78d6d
GuillaumeClement <guillaume.clement@etu.umontpellier.Fr>
2019-05-08 17:40:08 +0200

    reorganisation par struct

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./ressources/accueil/index');
+var usersRouter = require('./ressources/users/users');
 
 var app = express();
 
