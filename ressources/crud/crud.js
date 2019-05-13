53de89813a0f51cd05ea105f01061294ad789fe7
GuillaumeClement <guillaume.clement@etu.umontpellier.Fr>
2019-05-13 16:58:19 +0200

    test

--- ressources/crud/crud.js
+++ ressources/crud/crud.js
@@ -14,39 +14,53 @@ const pool = new Pool({
   ssl: true
 });
 
-pool.on('error', traitementErreur)
-
-function traitementErreur(err, client){
+pool.on('error', function (err, client){
 
   console.error('Unexpected error on idle client', err)
 
 }
 
 //acces a la base de donnee 
-router.get('/',queryDb)
 
-//cela marche seulement sur le heroku, pas en local (pas ssl en local)
+//cela marche seulement sur le heroku, pas en local (pas ssl en local) ?
+/*
+router.get('/', function (req, res, next) {
 
-function queryDb(req, res, next) {
-
-  pool.connect( (err, client, done) => {
+  pool.connect( function (err, client, done) {
     if (err) throw err
 
-    client.query('SELECT * FROM edition WHERE code = $1', ["WAR"], (err, res) => {
+    client.query('SELECT * FROM edition WHERE code = $1', ["WAR"], function(err, res)  {
 
-      done()
+      done()//ferme la connexion
 
       if (err) {
         console.log(err.stack)
       } else {
         console.log(res.rows)
-        res.send(res.rows)
+        res.send(res.rows)//heroku bug ici
       }
     })
 
   })
   
 }
+*/
+router.get('/', function (req, res, next) {
+    pool.connect(connectionString,function(err,client,done) {
+       if(err){
+           console.log("not able to get connection "+ err);
+           res.status(400).send(err);
+       } 
+       client.query('SELECT * FROM edition where code = $1', ["WAR"],function(err,result) {
+           done(); // closing the connection;
+           if(err){
+               console.log(err);
+               res.status(400).send(err);
+           }
+           res.status(200).send(result.rows);
+       });
+    });
+});
 
 
 module.exports = router
