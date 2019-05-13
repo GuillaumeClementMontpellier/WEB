69521e758a4cd7a149ce2a8f6c38349959b3292e
GuillaumeClement <guillaume.clement@etu.umontpellier.Fr>
2019-05-13 17:28:41 +0200

    test

--- ressources/crud/crud.js
+++ ressources/crud/crud.js
@@ -23,47 +23,24 @@ pool.on('error', function (err, client){
 //acces a la base de donnee 
 
 //cela marche seulement sur le heroku, pas en local (pas ssl en local) ?
-/*
+
 router.get('/', function (req, res, next) {
   pool.connect( function(err,client,done) {
-   if(err){
-     console.log("not able to get connection "+ err);
-     res.status(400).send(err);
-   } 
-   client.query('SELECT * FROM edition where code = $1', ["WAR"],function(err,result) {
-           done(); // closing the connection;
-           if(err){
-             console.log(err);
-             res.status(400).send(err);
-           }
-           res.status(200).send(result.rows);
-         });
- });
+    if(err){
+      console.log("not able to get connection "+ err);
+      res.status(400).send(err);
+    } 
+    client.query('SELECT * FROM edition where code = $1', ["WAR"],function(err,result) {
+      done(); // closing the connection;
+      if(err){
+       console.log(err);
+       res.status(400).send(err);
+     }
+     res.status(200).send(result.rows);
+   });
+  });
 });
-*/
-router.get('/', apiGet);
-function apiGet(req, res, next) {
 
-  pool.connect(reqGet);
-
-}
-
-function reqGet(err,client,done) {
- if(err){
-   console.log("not able to get connection "+ err);
-   res.status(400).send(err);
- } 
- client.query('SELECT * FROM edition where code = $1', ["WAR"],resGet);
-}
-
-function resGet(err,result) {
-  done(); // closing the connection;
-  if(err){
-   console.log(err);
-   res.status(400).send(err);
- }
- res.status(200).send(result.rows);
-}
 
 
 module.exports = router
