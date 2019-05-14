e5e99506f336c9dcbe65ec16a61ef56d5e559d3f
GuillaumeClement <guillaume.clement@etu.umontpellier.Fr>
2019-05-14 19:37:00 +0200

    crud api : debut implementation

--- ressources/crud/carteAPI.js
+++ ressources/crud/carteAPI.js
@@ -11,34 +11,78 @@ const pool = new Pool({
 	ssl: true
 });
 
-//GET
-app.get('/top', topReq)
-app.get('/nbr_comment', nbrReq)
-app.get('/comments', commentsReq)
-app.get('/types',typesReq)
-app.get('/sub_types',sTypesReq)
-app.get('/editions',editionReq)
-app.get('/modele',modeleReq)
-app.get('/:id_carte',carteInfoReq)
+//GET -------------------------
+//toutes les cartes ----------
+app.get('/by_top', topReq)
+app.get('/by_nbr_comment', nbrReq)
 
-//req un certains nombre de cartes, qui ont meuileur score de wilson
+//req un certains nombre de cartes, qui ont meilleur score de wilson (params : nbr[, offset, ])
 function topReq(req, res, next) {
 
-    if(!req.params) next(new Error(400))
-    if(!req.params.nbr) next(new Error(400))
+  if(!req.params) next(new Error(400));
+  if(typeof req.params.nbr != 'number') next(new Error(400));
 
-	pool.query('SELECT id, image_url FROM carte_var ', [], function(err,result) {
+  let q = 'SELECT id, image_url FROM carte_var WHERE carte_like_count(id) + carte_dislike_count(id) > 0 ORDER BY score(carte_like_count(id),carte_dislike_count(id)) LIMIT $1;';
+  let par = [req.params.nbr];
+
+  if(typeof req.params.offset == 'number'){
+    q += 'OFFSET $2'
+    par.push(req.params.offset)
+  }
+
+  if(req.params.desc){
+    q.replace('LIMIT', 'DESC LIMIT')
+  }
+
+  pool.query(q, par, function(err,result) {
+    done(); // closing the connection
+    if(err) {
+      res.status(400).send(err);
+    }
+    res.status(200).send(result.rows);
+  });
+}
+//req un certains nombre de cartes, qui ont le plus de comments
+function nbrReq(req, res, next) {
+
+  if(!req.params) next(new Error(400));
+  if(typeof req.params.nbr != 'number') next(new Error(400));
+
+  let q = 'SELECT id, image_url, count(*) as nbr FROM carte_var, comment WHERE comment.carte_id = carte_var.id ORDER BY nbr LIMIT $1;';
+  let par = [req.params.nbr];
+
+  if(typeof req.params.offset == 'number'){
+    q += 'OFFSET $2'
+    par.push(req.params.offset)
+  }
+
+  if(req.params.desc){
+    q.replace('LIMIT', 'DESC LIMIT')
+  }
+
+  pool.query(q, par, function(err,result) {
 
     done(); // closing the connection
 
     if(err) {
-    	console.log(err);
-    	res.status(400).send(err);
+      res.status(400).send(err);
     }
     res.status(200).send(result.rows);
-
   });
 }
+
+//tout les types/soustypes/modeles/editions de cartes -------------
+app.get('/types',typesReq)
+app.get('/sub_types',sTypesReq)
+app.get('/editions',editionReq)
+app.get('/modele',modeleReq)
+
+//infos sur une certaine carte (de la carte, ou ses comments) -----------------
+app.get('/:id_carte',carteInfoReq)
+app.get('/comments/:id_carte', commentsReq)
+
+
+
 
 
 
