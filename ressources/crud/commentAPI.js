0f33cf92a72d4fc516c5a1d61e4f0f435c6fdb03
GuillaumeClement <guillaume.clement@etu.umontpellier.Fr>
2019-05-18 14:52:07 +0200

    Fini api of crud

--- ressources/crud/commentAPI.js
+++ ressources/crud/commentAPI.js
@@ -13,7 +13,6 @@ const pool = new Pool({
 
 //GET
 //Specifique
-app.get('/:id',detailsComm)
 app.get('/reply/:id',listeReply)
 
 function detailsComm(req, res, next) {
@@ -87,7 +86,7 @@ function topReqComm(req, res, next) {
 
   let q = `SELECT comment_id, contenu, created, edited, carte_id, author_id, name_user FROM commentaire, user_profile 
   WHERE author_id=id_user ORDER BY score(comment_like_count(comment_id)+1,comment_dislike_count(comment_id)) LIMIT $1`
-  
+
   let par = [req.query.nbr]
 
   if(typeof req.query.offset === 'string'){
@@ -444,6 +443,8 @@ function deleteComm(req, res, next){
   })
 }
 
+//mis a la fin pour /:id qui reconnais tout
+app.get('/:id',detailsComm)
 
 
 module.exports = app
